c8e7ab3910342feaca645945179b706814cb0fb4
anmi404 <anolita@gmail.com>
2019-07-13 07:01:34 -0300

    Init
    mend

--- Clients/wifiesp32/wifiesp32.ino
+++ Clients/wifiesp32/wifiesp32.ino
@@ -1,17 +1,20 @@
 
 #include <WiFi.h>
-#include <WiFiUpd.h>
+#include <WiFiUdp.h>
+//#include <string.h>
 
 /* WiFi network name and password */
 // Your wirelless router ssid and password
 const char * ssid = "moto_2";
 const char * pwd = "31d7dd433fc0";
+//const char* ssid="dragino-18f0e4";
+//const char* password = "iotincendios";
 
 // IP address to send UDP data to.
 // it can be ip address of the server or
 // a network broadcast address
 // here is broadcast address
-const char * udpAddress = ""; // your pc ip
+const char * udpAddress = "192.168.43.3"; // your pc ip
 const int udpPort = 5005; //port server
 
 //create UDP instance
@@ -21,7 +24,7 @@ void setup(){
   Serial.begin(115200); // to monitor activity
  
   //Connect to the WiFi network
-   WiFi.begin(ssid, pwd);
+  WiFi.begin(ssid, pwd);
   Serial.println("");
 
   // Wait for connection
@@ -39,20 +42,28 @@ void setup(){
 }
 
 void loop(){
-  //data will be sent to server
-  uint8_t buffer[50] = "hello world";
+  //Temperature sensor
+  Serial.print("Temperatura: ");
+  // Convert raw temperature in F to Celsius degrees
+  float t = 33.5;
+  Serial.print(33.5);
+  Serial.println(" *C");
+  
+  //uint8_t
+  uint8_t buffer[50] = "33.5";
+  //sprintf((char*) buffer, "Temperatura: %f *C", t);
   //send hello world to server
   udp.beginPacket(udpAddress, udpPort);
-  udp.write(buffer, 11);
+  udp.write(buffer, 4);//buffer
   udp.endPacket();
-  memset(buffer, 0, 50);
+  //memset(buffer, 0, 50);
   //processing incoming packet, must be called before reading the buffer
-  udp.parsePacket();
+  //udp.parsePacket();
   //receive response from server, it will be HELLO WORLD
-  if(udp.read(buffer, 50) > 0){
-    Serial.print("Server to client: ");
-    Serial.println((char *)buffer);
-  }
+  //if(udp.read(buffer, 50) > 0){
+  //  Serial.print("Server to client: ");
+  //  Serial.println((char *)buffer);
+  //}
   //Wait for 1 second
   delay(500);
 }
